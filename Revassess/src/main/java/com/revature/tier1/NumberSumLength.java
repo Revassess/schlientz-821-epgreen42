56abe061fe57fccc8b23c6da55f15bf48ce4fb75
epgreen42 <epgreen42@gmail.com>
2020-08-04 14:57:25 -0500

    Tier 1

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,14 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		
+		String numToString = String.valueOf(num);
+		String[] eachDigit = numToString.split("");
+		double power = eachDigit.length;
+		long newNum = 0L;
+		for(String digit : eachDigit) {
+			newNum += (long) Math.pow(Long.parseLong(digit), power);
+		}
+		return (newNum == num);
 	}
 }
